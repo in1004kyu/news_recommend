d0617f8fc05ff748b14586a48ec912067b83bce1
inkyu <in10004kyu@gmail.com>
2013-04-28 13:23:34 +0900

    add naive op

---
+++ naive/naiveBaysian.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+struct vector {
+	int id;
+	int count;
+};
+
+struct vector_list{
+	int length;
+	struct vector* vectors;
+};
+
+float nve(struct vector_list dvl, struct vector_list pdvl_subset,int pdvl_full_length);
