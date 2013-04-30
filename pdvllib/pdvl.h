ae9e36075ed5c2d43572cd5dcfbee4fe3fb56c20
dudnwjs <dudnwjs@gmail.com>
2013-04-30 13:58:50 +0900

    modified pdvl.h pdvl_update_count define

--- pdvllib/pdvl.h
+++ pdvllib/pdvl.h
@@ -36,7 +36,7 @@ typedef unsigned int pdvl_count_t;
  * API list
  */
 pdvl_status_t pdvl_open(pdvl_ctx_t *pctx,char *filename);
-pdvl_status_t pdvl_update_tid_count(pdvl_ctx_t *pctx, pdvl_tid_t dv_tid, pdvl_count_t dv_count);
+pdvl_status_t pdvl_update_tid_count(pdvl_ctx_t *pctx, pdvl_tid_t doc_tid, pdvl_count_t doc_count);
 pdvl_status_t pdvl_close(pdvl_ctx_t *pctx);
-pdvl_tid_t pdvl_get_count(pdvl_ctx_t* pctx, pdvl_tid_t tid);
+pdvl_count_t pdvl_get_count(pdvl_ctx_t* pctx, pdvl_tid_t tid);
 
