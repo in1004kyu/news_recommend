84fa2681b91264fb9af924ddd30a86dc60e1fef3
Simon Kim <spamhere@kaoma.net>
2013-05-09 09:51:43 +0900

    comments to d2vtest source: test.c

--- src/d2vlib/test.c
+++ src/d2vlib/test.c
@@ -8,12 +8,14 @@ int main()
 	d2v_vector_t doc_vec;
 	int i;
 
+	// Open input text file: test.txt
 	ret = d2v_open(&d2v_ctx,"test.txt");
 	if( ret != D2V_STATUS_SUCCESS ){
 		printf("d2v open error!\n");
 		return 0;
 	}
 
+	// Convert the input to it's document vector representation: doc_vec
 	d2v_get_document_vector(&d2v_ctx, &doc_vec);
 
 	for( i = 0; i < doc_vec.length; i++){
@@ -22,6 +24,7 @@ int main()
 				doc_vec.element[i].count);
 	}
 
+	// Cleanup
 	d2v_free_vector(&doc_vec);
 	d2v_close(&d2v_ctx);
 
