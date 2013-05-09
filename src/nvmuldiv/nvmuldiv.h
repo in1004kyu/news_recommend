840ef8549ad7e86b47c246f5aa77fc6e2ebf3008
Simon Kim <spamhere@kaoma.net>
2013-05-09 11:11:59 +0900

    list of functions to be deprecated soon

--- src/nvmuldiv/nvmuldiv.h
+++ src/nvmuldiv/nvmuldiv.h
@@ -6,8 +6,6 @@ extern "C" {
 #endif
 
 float nvmuldiv( float numerators[], float denominators[], int len);
-float nvmuldiv_cuda_thrust( float numerators[], float denominators[], int len);
-float nvmuldiv_cuda( float numerators[], float denominators[], int len);
 
 // bulk processing
 // Multi-segment bulk processing of nvmuldiv()
@@ -20,12 +18,15 @@ float nvmuldiv_cuda( float numerators[], float denominators[], int len);
 // 	4 segments ( = 256 / 64 )
 // 	numerators at segment 0 : n[0] ~ n[63]
 int nvmuldiv_seg( float n[], float d[], int len, float r[], int len_r );
-// cuda version of nvmuldiv_seg()
-int nvmuldiv_seg_cuda( float n[], float d[], int len, float r[], int len_r );
 
 // low level CUDA reduce functions
 int reduce_mul( float *v, int len, float *result );
 int reduce_mul_seg( float v[], int len, float r[], int len_r );
+
+// To be deprecated
+float nvmuldiv_cuda( float numerators[], float denominators[], int len);
+float nvmuldiv_cuda_thrust( float numerators[], float denominators[], int len);
+int nvmuldiv_seg_cuda( float n[], float d[], int len, float r[], int len_r );
 #ifdef __cplusplus 
 }
 int vdiv_cuda( float v1[], float v2[], float vr[], int len );
