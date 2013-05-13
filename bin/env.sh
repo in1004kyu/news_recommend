c2ecc4e25ca4e99f136330cad65fd26120b425dd
Simon Kim <spamhere@kaoma.net>
2013-05-13 09:35:23 +0900

    Makefile: removed bash for loop statement and put .PHOTNY and subdirs instead

--- bin/env.sh
+++ bin/env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:../lib
 echo "=== Make sure you executed as 'source $0' in bash ==="
 echo "Now you are ready to execute binaries in this directory"
