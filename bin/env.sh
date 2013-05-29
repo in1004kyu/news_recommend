0a8f21493f5594dca9438b77ad2a6f3294dd15b7
inkyu <in10004kyu@gmail.com>
2013-05-29 10:40:10 +0900

    training

--- bin/env.sh
+++ bin/env.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
-export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:../lib
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${PWD}/../lib
+#export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:../lib
 echo "=== Make sure you executed as 'source $0' in bash ==="
 echo "Now you are ready to execute binaries in this directory"
